a7fd2de04b52562ee92023e59ac62ceff9a08bd7
behnam <behnamtakta71@gmail.com>
2019-01-21 17:33:08 +0330

    liking posts done

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,17 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Post;
+use App\User;
 use Carbon\Carbon;
 use Validator;
 use Illuminate\Http\Request;
 use Intervention\Image\Facades\Image;
+
 class PostController extends Controller
 {
 
     public function sendPost(Request $request)
     {
-        $imageUrl="";
-        if($request->imageUrl) {
+        $imageUrl = "";
+        if ($request->imageUrl) {
             $data = $request->imageUrl;
             list($type, $data) = explode(';', $data);
             list(, $data) = explode(',', $data);
@@ -26,34 +28,34 @@ class PostController extends Controller
             $imageUrl = public_path() . "/uploads/" . auth()->user()->userName . "/post-image/" . $image_name;
             file_put_contents($imageUrl, $data);
         }
-        $postType="post";
-        $roll=[
+        $postType = "post";
+
+        $roll = [
             "user_id" => "required|numeric",
-            "body" =>  "required|min:1|max:5000|",
-            "mood" =>  "required|alpha|max:10",
-            "location" =>"alpha|max:30"
+            "body" => "required|min:1|max:5000|",
+            "mood" => "required|alpha|max:10",
+            "location" => "alpha|max:30",
         ];
 
-        if ($request->postType == "vip"){
-            if (auth()->user()->credit >= 1000){
-                $postType="vip";
-            }
-            else{
+        if ($request->postType == "vip") {
+            if (auth()->user()->credit >= 1000) {
+                $postType = "vip";
+            } else {
                 alert()->success('خر خودتی!')->persistent('فهمیدی؟');
                 return back();
             }
         }
-        if(!$imageUrl==""){
-            $img=strstr($imageUrl,'/upload');
-        }else{
-            $img=null;
+        if (!$imageUrl == "") {
+            $img = strstr($imageUrl, '/upload');
+        } else {
+            $img = null;
         }
 
-        Validator::make($request->all(),$roll);
+        Validator::make($request->all(), $roll);
         Post::create([
             'user_id' => $request->user_id,
-            'body' =>$request->body,
-            'mood' =>$request->mood,
+            'body' => $request->body,
+            'mood' => $request->mood,
             'location' => $request->Location,
             'imageUrl' => $img,
             'postType' => $postType,
@@ -62,13 +64,14 @@ class PostController extends Controller
         return redirect()->back();
 
     }
+
     public function editPost(Post $post)
     {
         $validator = Validator::make(\request()->all(), [
             "user_id" => "required|numeric",
-            "body" =>  "required|min:1|max:5000",
-            "mood" =>  "alpha|max:10",
-            "location" =>"string|max:30"
+            "body" => "required|min:1|max:5000",
+            "mood" => "alpha|max:10",
+            "location" => "string|max:30"
         ]);
         if ($validator->fails()) {
             alert()->warning('توی وارد کردن اطلاعات دقت کن')->persistent('فهمیدم');
@@ -76,11 +79,10 @@ class PostController extends Controller
         }
 
         $postType = "post";
-        if (request('postType') == "vip"){
-            if (auth()->user()->credit >= 1000){
-                $postType="vip";
-            }
-            else{
+        if (request('postType') == "vip") {
+            if (auth()->user()->credit >= 1000) {
+                $postType = "vip";
+            } else {
                 alert()->success('خر خودتی!')->persistent('فهمیدی؟');
                 return back();
             }
@@ -95,24 +97,36 @@ class PostController extends Controller
         alert()->success('پست مورد نظر با موفقیت ویرایش شد', $title)->persistent('اوکی');
         return back();
     }
+
     public function removePost(Post $post)
     {
         $post->delete();
 
         $title = " عزیز!" . $post->user->firstName;
-        alert()->success('پست مورد نظر با موفقیت حذف شد', $title )->persistent('اوکی');
+        alert()->success('پست مورد نظر با موفقیت حذف شد', $title)->persistent('اوکی');
         return back();
     }
 
     public function likePost(Request $request)
     {
 
-        $like=[
-            "counte" => 0,
-            "user_id" => [],
-        ];
-
-
-
+        $likeUser = Post::whereId($request->post_id)->get()->first()->likeCount;
+        $functur=$request->functur;
+        if (in_array($functur, $likeUser['id'])) {
+            $key = array_search($functur, $likeUser['id']);
+            unset($likeUser['id'][$key], $functur);
+            $likeUser['count'] = $likeUser['count'] - 1;
+            Post::whereId($request->post_id)->update([
+                'likeCount' => json_encode($likeUser),
+            ]);
+            return $likeUser['count'];
+        } else {
+            $likeUser['id'][]=json_decode($functur);
+            $likeUser['count'] = $likeUser['count']+1;
+            Post::whereId($request->post_id)->update([
+                'likeCount' => json_encode($likeUser),
+            ]);
+            return $likeUser['count'];
+            }
+        }
     }
-}
--- app/Post.php
+++ app/Post.php
@@ -6,7 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    protected $guarded=[];
+
+    protected $fillable=[
+        'user_id','body','imageUrl','mood','likeCount','commentCount','shareCount','postType','location'
+    ];
+    protected $table = 'posts';
+
+    protected $casts = [
+        'likeCount' => 'array'
+    ];
+
     public function user()
     {
         return $this->belongsTo(User::class);
